e2c3cbe5af89e11f8a9b2968939f73c9798331a5
ehidanchavez67 <ehidanchavez@unam.edu>
2026-03-28 04:49:46 +0000

    Se completo exitosamente

--- cola/cola.c
+++ cola/cola.c
@@ -1,78 +1,9 @@
-#include <stdlib>
-#include <stdbool>
+#include "../lista/lista.h"
+#include <stdlib.h>
+#include <stdbool.h>
 #include "cola.h"
 
-Cola* cola_crear()
-{
-    return lista_crear();
-}
 
-int cola_vacia(Cola* cola)
-{
-    return lista_vacia((Lista*)cola);
-}
-
-void cola_enqueue(Cola* cola, int dato)
-{
-    lista_insertar_tail((Lista*)cola, dato);
-}
-
-int cola_dequeue(Cola* cola)
-{
-    return lista_eliminar_head((Lista*)cola);
-}
-
-int cola_frente(Cola* cola)
-{
-    if (cola_vacia(cola)) return -1;
-    return ((Lista*)cola)->head->dato;
-}
-
-void cola_destruir(Cola* cola)
-{
-    lista_destruir((Lista*)cola);
-}
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 Cola* cola_crear()
 {
     return (Cola*)lista_crear();
@@ -103,4 +34,42 @@ void cola_destruir(Cola* cola)
 {
     lista_destruir((Lista*)cola);
 }
-*/
+// A partir de lista.h se renombra con typedef a cola.h y se utilizan o "reciclan" las funciones ya creadas para la lista
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
